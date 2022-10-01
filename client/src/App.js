5b6b68256048c528468ab3ace3a6c220c11365c1
David Mendoza <davidleomendoza300@gmail.com>
2022-10-01 11:44:51 -0500

    Push client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import img1 from "./img/3551.jpg";
 function App() {
   return (
     <div className="App">
       <h1>Henry Videogames</h1>
+      <div className = "links"></div>
+      <img className="App-header" src ={img1} alt="Fondo videojuegos"/> 
     </div>
   );
 }
